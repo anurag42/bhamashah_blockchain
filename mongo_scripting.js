ff746d26b2a4b20698cf115a0cae94e96e2266a0
viswanath12 <viswanath@bigstylist.com>
2017-12-02 23:43:48 +0530

    changes

--- mongo_scripting.js
+++ mongo_scripting.js
@@ -1,4 +1,100 @@
 db.kisaans.insert({
+    "_id" : ObjectId("767676767546354234657453"),
+    "mobile" : "8639855513",
+    "ethereumAddress" : "66879",
+    "aadhar" : "456387624562",
+    "__v" : 0,
+    "name" : "Arjun Acharya"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("758546378475673874655342"),
+    "mobile" : "9160680940",
+    "ethereumAddress" : "66879",
+    "aadhar" : "980387297645",
+    "__v" : 0,
+    "name" : "Manoj Agarwal"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("848375657483273252342564"),
+    "mobile" : "8178976697",
+    "ethereumAddress" : "66879",
+    "aadhar" : "234178493724",
+    "__v" : 0,
+    "name" : "Ankur Khatri"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("654293448346532336593246"),
+    "mobile" : "9769128135",
+    "ethereumAddress" : "66879",
+    "aadhar" : "983474527818",
+    "__v" : 0,
+    "name" : "Abhishek Lahari"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("834732727301023737487570"),
+    "mobile" : "8655123902",
+    "ethereumAddress" : "66879",
+    "aadhar" : "743637264567",
+    "__v" : 0,
+    "name" : "Rakesh"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("238642972854653487286236"),
+    "mobile" : "9987564534",
+    "ethereumAddress" : "66879",
+    "aadhar" : "871847587364",
+    "__v" : 0,
+    "name" : "Sam Dara"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("048578473463462352785823"),
+    "mobile" : "8749382647",
+    "ethereumAddress" : "66879",
+    "aadhar" : "565432987654",
+    "__v" : 0,
+    "name" : "Naveen Dalal"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("294757609580423328921399"),
+    "mobile" : "8749382647",
+    "ethereumAddress" : "66879",
+    "aadhar" : "145674843956",
+    "__v" : 0,
+    "name" : "Vinay Amin"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("348834638478298374896563"),
+    "mobile" : "8749382647",
+    "ethereumAddress" : "66879",
+    "aadhar" : "987646532654",
+    "__v" : 0,
+    "name" : "Ashish Chawla"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("362836449182638264237254"),
+    "mobile" : "8749382647",
+    "ethereumAddress" : "66879",
+    "aadhar" : "754867743243",
+    "__v" : 0,
+    "name" : "Parth Chawla"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("827397248624896375523375"),
+    "mobile" : "8749382647",
+    "ethereumAddress" : "66879",
+    "aadhar" : "985646329451",
+    "__v" : 0,
+    "name" : "Mayank Chawla"
+});
+db.kisaans.insert({
+    "_id" : ObjectId("237948264892634782673462"),
+    "mobile" : "8749382647",
+    "ethereumAddress" : "66879",
+    "aadhar" : "338975432145",
+    "__v" : 0,
+    "name" : "Aditya Dara"
+});
+db.kisaans.insert({
     "_id" : ObjectId("786876777576786876777576"),
     "mobile" : "8639855513",
     "ethereumAddress" : "66879",
